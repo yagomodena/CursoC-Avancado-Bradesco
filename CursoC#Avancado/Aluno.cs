4f28c587dd576de579c234058850cb38495cb472
Yago <Yago@DESKTOP-59265J7>
2023-08-17 23:17:55 -0300

    Adicionar arquivos de projeto.

---
+++ CursoC#Avancado/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoC_Avancado
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Idade;
+        public int Serie;
+
+        public string Retorno()
+        {
+            return string.Format($"O aluno {Nome} tem {Idade} e pertence a serie {Serie}");
+        }
+
+        public void RetornoConsole()
+        {
+            Console.WriteLine(Retorno());
+        }
+    }
+}
---
+++ CursoC#Avancado/Program.cs
@@ -0,0 +1,14 @@
+﻿using CursoC_Avancado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno();
+        aluno.Nome = "Yago";
+        aluno.Idade = 21;
+        aluno.Serie = 3;
+
+        aluno.RetornoConsole();
+    }
+}
