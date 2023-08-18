c10016c8f1fb881167c7305444b990c3dd7d2dac
Yago Modena <modenayago@yahoo.com>
2023-08-18 00:06:49 -0300

    Finalização classe carro

---
+++ CursoC#Avancado/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoC_Avancado
+{
+    public class Carro
+    {
+        public string Marca;
+        public string Nome;
+        public int ano;
+        public int Serie;
+    }
+}
