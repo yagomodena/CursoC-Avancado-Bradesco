6c8e5071cb67c4113b46f53dd7a4129281358f79
Yago Modena <modenayago@yahoo.com>
2023-08-18 00:06:19 -0300

    Criação da classe Professor Fim do curso.

---
+++ CursoC#Avancado/Professor.cs
@@ -0,0 +1,42 @@
+﻿namespace CursoC_Avancado
+{
+    public class Professor
+    {
+        protected readonly float salariomaximo;
+        float salarioatual;
+
+        public Professor(float salarioMaximo)
+        {
+            salariomaximo = salarioMaximo;
+        }
+
+        protected float ModificarSalario(float Modf)
+        {
+            float novoSalario = salarioatual + Modf;
+
+            if(novoSalario < 0 )
+            {
+                salarioatual = 0;
+            }else if( novoSalario > salariomaximo)
+            {
+                salarioatual = salariomaximo;
+            }
+            else
+            {
+                salarioatual = novoSalario;
+            }
+
+            return salarioatual;
+        }
+
+        public float AumentaSalario()
+        {
+            return ModificarSalario(+500);
+        }
+
+        public float DiminuiSalario()
+        {
+            return ModificarSalario(-500);
+        }
+    }
+}
